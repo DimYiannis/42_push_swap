21d07b6e7fc28030b98bc5b821b6a63198f94485
yiannis <107484245+DimYiannis@users.noreply.github.com>
2025-11-16 17:32:36 +0100

    created node struct and function to create a circular double linked list

---
+++ push_swap.c
@@ -0,0 +1,44 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   push_swap.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: ydimitra <ydimitra@student.42.f>           +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/11/16 16:20:40 by ydimitra          #+#    #+#             */
+/*   Updated: 2025/11/16 17:30:40 by ydimitra         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+
+#include "header.h"
+
+
+void createlist(int argc,char *argv[], node **stack_a)
+{
+  struct node *tmp;
+  struct node *newnode;
+  int i;
+  
+  i = 2;
+  *stack_a = (struct node *)malloc(sizeof(struct node));
+  if (!stack_a)
+    return NULL;
+  (*stack_a)->data = ft_atoi(argv[1]);
+  (*stack_a)->next = *stack_a;
+  (*stack_a)->prev = *stack_a;
+  tmp = *stack_a;
+  while (i < argc)
+  {
+    newnode = (struct node *)malloc(sizeof(struct node));
+    if (!newnode)
+      return (NULL);
+    newnode->data = ft_atoi(argv[i]);
+    newnode->next = *stack_a;
+    newnode->prev = *tmp;
+    tmp->next = newnode;
+    (*stack_a)->prev = newnode;
+    tmp = tmp->next;
+    i++;
+  }
+ }
