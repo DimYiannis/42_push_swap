4be181721eb9058681e8686baf366f9198071c88
yiannis <107484245+DimYiannis@users.noreply.github.com>
2025-11-16 17:47:09 +0100

    deletion of libft

--- header.h
+++ header.h
@@ -6,14 +6,15 @@
 /*   By: ydimitra <ydimitra@student.42.f>           +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/11/16 16:16:02 by ydimitra          #+#    #+#             */
-/*   Updated: 2025/11/16 16:30:43 by ydimitra         ###   ########.fr       */
+/*   Updated: 2025/11/16 17:43:42 by ydimitra         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 
 #ifndef HEADER_H
 # define HEADER_Hi
-# include "libft/libft.h"
+# include "printf/libft/libft.h"
+# include "printf/ft_printf_bonus.h"
 
 typedef struct node
 {
