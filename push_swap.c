db81ea95a566290e096235ce85d9cefce7a34df9
Yiannis Dimitrakopoulos <ydimitra@f0r4s24.codam.nl>
2025-11-20 14:17:49 +0100

    guard clauses, free stack in case a malloc goes bad and also check for invalid input and we dont crash, for example an input like ./a.out 1 2 NULL 3,

--- push_swap.c
+++ push_swap.c
@@ -13,6 +13,23 @@
 
 #include "header.h"
 
+void	free_up(t_node *head)
+{
+  t_node *node;
+  t_node *tmp;
+
+  if (!head)
+    return;
+  node = head->next;
+  head->prev->next = NULL;
+	while (node)
+  {
+    tmp = node->next;
+    free(node);
+    node = tmp;
+  }
+  free(head);
+}
 
 void createlist(int n, char *arr[], t_stack *stack)
 {
@@ -21,24 +38,46 @@ void createlist(int n, char *arr[], t_stack *stack)
   int i;
   
   i = 2;
+  if (n < 2)
+  {
+    ft_printf("Error\n");
+    *stack = NULL;
+    return;
+  }
   *stack = malloc(sizeof(t_node));
   if (!(*stack))
+  {
     ft_printf("Error\n");
+    *stack = NULL;
+    return;
+  }
   (*stack)->data = ft_atoi(arr[1]);
   (*stack)->next = *stack;
   (*stack)->prev = *stack;
   tmp = *stack;
   while (i < n)
   {
+    if(!arr[i])
+    {
+      ft_printf("Error\n");
+      free_up(*stack);
+      *stack = NULL;
+      return;
+    }
     newnode = malloc(sizeof(t_node));
     if (!newnode)
-       ft_printf("Error\n");
+    {
+      ft_printf("Error\n");
+      free_up(newnode);
+      *stack = NULL;
+      return;
+    }
     newnode->data = ft_atoi(arr[i]);
     newnode->next = *stack;
     newnode->prev = tmp;
     tmp->next = newnode;
     (*stack)->prev = newnode;
-    tmp = tmp->next;
+    tmp = newnode;
     i++;
   }
  }
