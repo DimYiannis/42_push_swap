3d357c4265556250f59d092100752218b9465a7f
yiannis <107484245+DimYiannis@users.noreply.github.com>
2025-11-26 17:03:36 +0100

    minor change

--- push_swap.c
+++ push_swap.c
@@ -6,7 +6,7 @@
 /*   By: ydimitra <ydimitra@student.42.f>           +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/11/16 16:20:40 by ydimitra          #+#    #+#             */
-/*   Updated: 2025/11/26 16:45:17 by ydimitra         ###   ########.fr       */
+/*   Updated: 2025/11/26 16:51:22 by ydimitra         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -36,10 +36,8 @@ void push_swap(t_stack *stack_a, t_stack *stack_b)
 
   while (stack_size(stack_b) < 3)
     push_to_b(stack_a, stack_b);
-  //set max min and sort the 2 nodes
   if (*stack_b->data > *stack_b->next->data)
     shift_up(*stack_b);
-
   while (stack_size(stack_a) > 0)
   {
     // find cheapest
