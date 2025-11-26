ef929503545cbf2f4ac79786e691640ea96a8ee3
Yiannis Dimitrakopoulos <ydimitra@f0r1s11.codam.nl>
2025-11-26 16:39:11 +0100

    moved around some functions for more space and clarity

--- actions.c
+++ actions.c
@@ -6,7 +6,7 @@
 /*   By: ydimitra <ydimitra@student.42.f>           +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/11/18 15:39:12 by ydimitra          #+#    #+#             */
-/*   Updated: 2025/11/18 15:39:35 by ydimitra         ###   ########.fr       */
+/*   Updated: 2025/11/26 16:06:57 by ydimitra         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -58,50 +58,4 @@ void push_back_to_a(t_stack *stack_a, t_stack *stack_b)
   }
 }
 
-void sort_three(t_stack *stack_a)
-{
-  int first; 
-  int second; 
-  int third;
 
-  if (!*stack_a || (*stack_a)->next == *stack_a)
-      return;
-  if ((*stack_a)->next->next == *stack_a && (*stack_a->data < stack_a->next->data))
-  {
-    shift_up(stack_a);
-    ft_printf("ra\n");
-  }
-    first = (*stack_a)->data;
-    second = (*stack_a)->next->data;
-    third = (*stack_a)->next->next->data;
-
-    if (first > second && second < third && first < third)
-    {
-        swap_first_sec(stack_a);
-        ft_printf("sa\n");
-    }
-    else if (first > second && second > third)
-    {
-        swap_first_sec(stack_a);
-        ft_printf("sa\n");
-        shift_down(stack_a);
-        ft_printf("rra\n");
-    }
-    else if (first > second && second < third && first > third)
-    {
-        shift_up(stack_a);
-        ft_printf("ra\n");
-    }
-    else if (first < second && second > third && first < third)
-    {
-        swap_first_sec(stack_a);
-        ft_printf("sa\n");
-        shift_up(stack_a);
-        ft_printf("ra\n");
-    }
-    else if (first < second && second > third && first > third)
-    {
-        shift_down(stack_a);
-        ft_printf("rra\n");
-    }
-}
---
+++ sorting.c
@@ -0,0 +1,61 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   sorting.c                                          :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: ydimitra <ydimitra@student.42.f>           +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/11/26 16:06:22 by ydimitra          #+#    #+#             */
+/*   Updated: 2025/11/26 16:06:52 by ydimitra         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "header.h"
+
+void sort_three(t_stack *stack_a)
+{
+  int first; 
+  int second; 
+  int third;
+
+  if (!*stack_a || (*stack_a)->next == *stack_a)
+      return;
+  if ((*stack_a)->next->next == *stack_a && (*stack_a->data < stack_a->next->data))
+  {
+    shift_up(stack_a);
+    ft_printf("ra\n");
+  }
+    first = (*stack_a)->data;
+    second = (*stack_a)->next->data;
+    third = (*stack_a)->next->next->data;
+
+    if (first > second && second < third && first < third)
+    {
+        swap_first_sec(stack_a);
+        ft_printf("sa\n");
+    }
+    else if (first > second && second > third)
+    {
+        swap_first_sec(stack_a);
+        ft_printf("sa\n");
+        shift_down(stack_a);
+        ft_printf("rra\n");
+    }
+    else if (first > second && second < third && first > third)
+    {
+        shift_up(stack_a);
+        ft_printf("ra\n");
+    }
+    else if (first < second && second > third && first < third)
+    {
+        swap_first_sec(stack_a);
+        ft_printf("sa\n");
+        shift_up(stack_a);
+        ft_printf("ra\n");
+    }
+    else if (first < second && second > third && first > third)
+    {
+        shift_down(stack_a);
+        ft_printf("rra\n");
+    }
+}
