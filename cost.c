dc0e9b73a44e3954f0e3cc493e1ea5383371ea8b
Yiannis Dimitrakopoulos <ydimitra@f0r1s11.codam.nl>
2025-11-27 18:39:52 +0100

    assigning median value and calculating how far each node is from the head

--- cost.c
+++ cost.c
@@ -6,7 +6,7 @@
 /*   By: ydimitra <ydimitra@student.42.f>           +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/11/27 18:08:57 by ydimitra          #+#    #+#             */
-/*   Updated: 2025/11/27 18:16:14 by ydimitra         ###   ########.fr       */
+/*   Updated: 2025/11/27 18:38:11 by ydimitra         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -22,15 +22,49 @@ int distance_to_head(t_stack *stack)
 {
   int distance;
   int stack_len;
+  int median;
+  int i;
+  t_node *tmp;
 
+  tmp = stack;
+  i = 0;
   stack_len = (stack_size(&stack));
   distance = 0;
+  median = stack_len / 2;
   if (stack_len % 2 = 0)
   {
-  //distance = median - i
+    while (i <= median)
+    {
+      tmp->distance = i;
+      tmp = tmp->next;
+      i++;
+    }
+    i = 0;
+    while (i < median)
+    {
+      tmp->distance = i;
+      tmp = tmp->prev;
+      i++;
+    }
   }
   else if (stack_len % 2 != 0)
   {
-// node->next->distance = median
+    // node->next->distance = median
+     while (i <= median)
+    {
+      tmp->distance = i;
+      tmp = tmp->next;
+      if (i == median)
+        tmp->next->distance = median;
+      i++;
+    }
+    i = 0;
+    while (i < median)
+    {
+      tmp->distance = i;
+      tmp = tmp->prev;
+      i++;
+    }
+
   }
 }
