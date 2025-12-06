12136372b2214e384d9cf9b78a942d5bd442ef90
Yiannis Dimitrakopoulos <ydimitra@f0r2s9.codam.nl>
2025-12-06 17:04:01 +0100

    improved sort three function, simpified logic

--- sorting.c
+++ sorting.c
@@ -6,7 +6,7 @@
 /*   By: ydimitra <ydimitra@student.42.f>           +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/11/26 16:06:22 by ydimitra          #+#    #+#             */
-/*   Updated: 2025/11/26 16:47:38 by ydimitra         ###   ########.fr       */
+/*   Updated: 2025/12/06 17:01:57 by ydimitra         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,49 +14,25 @@
 
 void sort_three(t_stack *stack_a)
 {
-  int first; 
-  int second; 
-  int third;
+  t_node *max;
 
   if (!*stack_a || (*stack_a)->next == *stack_a)
       return;
-  if ((*stack_a)->next->next == *stack_a && ((*stack_a)->data < (*stack_a)->next->data))
+  max = max_node(*stack_a);
+  if (*stack_a == max)
   {
     shift_up(stack_a);
     ft_printf("ra\n");
   }
-    first = (*stack_a)->data;
-    second = (*stack_a)->next->data;
-    third = (*stack_a)->next->next->data;
-
-    if (first > second && second < third && first < third)
-    {
-        swap_first_sec(stack_a);
-        ft_printf("sa\n");
-    }
-    else if (first > second && second > third)
-    {
-        swap_first_sec(stack_a);
-        ft_printf("sa\n");
-        shift_down(stack_a);
-        ft_printf("rra\n");
-    }
-    else if (first > second && second < third && first > third)
-    {
-        shift_up(stack_a);
-        ft_printf("ra\n");
-    }
-    else if (first < second && second > third && first < third)
-    {
-        swap_first_sec(stack_a);
-        ft_printf("sa\n");
-        shift_up(stack_a);
-        ft_printf("ra\n");
-    }
-    else if (first < second && second > third && first > third)
+  else if ((*stack_a)->next == max)
     {
         shift_down(stack_a);
         ft_printf("rra\n");
+    }
+    if ((*stack_a)->data > (*stack_a)->next->data)
+    {
+        swap_first_sec(stack_a);
+        ft_printf("sa\n");
     }
 }
 
