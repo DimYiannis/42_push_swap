a556f84134ea1651ab1fac10a54eda0334d85210
Yiannis Dimitrakopoulos <ydimitra@f0r2s9.codam.nl>
2025-12-06 17:28:07 +0100

    norm

--- helpers.c
+++ helpers.c
@@ -3,129 +3,127 @@
 /*                                                        :::      ::::::::   */
 /*   helpers.c                                          :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: ydimitra <ydimitra@student.42.f>           +#+  +:+       +#+        */
+/*   By: ydimitra <ydimitra@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/11/16 19:25:19 by ydimitra          #+#    #+#             */
-/*   Updated: 2025/11/27 18:25:45 by ydimitra         ###   ########.fr       */
+/*   Updated: 2025/12/06 17:24:41 by ydimitra         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "header.h"
 
-
-t_node *deletion(t_stack *stack)
+t_node	*deletion(t_stack *stack)
 {
-  t_node *tmp;
-  t_node *last;
- 
-  if (!stack || !*stack)
-    return (NULL);
-  tmp = *stack;
-  if (tmp->next == tmp)
-  {
-    *stack = NULL;
-    tmp->next = tmp->prev = tmp;
-    return tmp;
-  }
-  last = tmp->prev;
+	t_node	*tmp;
+	t_node	*last;
 
-  last->next = tmp->next;
-  tmp->next->prev = last;
-
-  *stack = tmp->next;
-
-  tmp->next = tmp->prev = tmp;
-  return (tmp);
+	if (!stack || !*stack)
+		return (NULL);
+	tmp = *stack;
+	if (tmp->next == tmp)
+	{
+		*stack = NULL;
+		tmp->next = tmp;
+		tmp->prev = tmp;
+		return (tmp);
+	}
+	last = tmp->prev;
+	last->next = tmp->next;
+	tmp->next->prev = last;
+	*stack = tmp->next;
+	tmp->next = tmp;
+	tmp->prev = tmp;
+	return (tmp);
 }
 
-void insert(t_stack *stack, t_node *newnode)
+void	insert(t_stack *stack, t_node *newnode)
 {
-  t_node *last;
+	t_node	*last;
 
-  if (!stack || !newnode)
-    return;
-   if (!*stack)
-  {
-    newnode->next = newnode;
-    newnode->prev = newnode;
-    *stack = newnode;
-    return;
-  }
-  last = (*stack)->prev;
-  newnode->next = *stack;
-  newnode->prev = last;
-  last->next = newnode;
-  (*stack)->prev = newnode;
-  *stack = newnode;
+	if (!stack || !newnode)
+		return ;
+	if (!*stack)
+	{
+		newnode->next = newnode;
+		newnode->prev = newnode;
+		*stack = newnode;
+		return ;
+	}
+	last = (*stack)->prev;
+	newnode->next = *stack;
+	newnode->prev = last;
+	last->next = newnode;
+	(*stack)->prev = newnode;
+	*stack = newnode;
 }
 
-void displaystack(t_stack stack)
+void	displaystack(t_stack stack)
 {
-    t_node *tmp;
+	t_node	*tmp;
 
-    if (!stack)
-        return;
-    tmp = stack;
-    while (1)
-    {
-        ft_printf("%d\n", tmp->data);
-        tmp = tmp->next;
-        if (tmp == stack)
-            break;
-    }
+	if (!stack)
+		return ;
+	tmp = stack;
+	while (1)
+	{
+		ft_printf("%d\n", tmp->data);
+		tmp = tmp->next;
+		if (tmp == stack)
+			break ;
+	}
 }
 
-void createlist(int n, char *arr[], t_stack *stack)
+void	createlist(int n, char *arr[], t_stack *stack)
 {
-  t_node *tmp;
-  t_node *newnode;
-  int i;
-  
-  i = 1;
-  if(!arr[i] || !is_number(arr[i]))
-  {
-      ft_printf("Error\n");
-      free_up(*stack);
-      *stack = NULL;
-      return;
-  }
-  *stack = malloc(sizeof(t_node));
-  if (!(*stack))
-  {
-    ft_printf("Error\n");
-    *stack = NULL;
-    return;
-  }
-  (*stack)->data = ft_atoi(arr[1]);
-  (*stack)->distance = 0;
-  (*stack)->next = *stack;
-  (*stack)->prev = *stack;
-  tmp = *stack;
-  i = 2;
-  while (i < n)
-  {
-    if(!arr[i] || !is_number(arr[i]))
-    {
-      ft_printf("Error\n");
-      free_up(*stack);
-      *stack = NULL;
-      return;
-    }
-    newnode = malloc(sizeof(t_node));
-    if (!newnode)
-    {
-      ft_printf("Error\n");
-      free_up(*stack);
-      *stack = NULL;
-      return;
-    }
-    newnode->data = ft_atoi(arr[i]);
-    newnode->distance = 0;
-    newnode->next = *stack;
-    newnode->prev = tmp;
-    tmp->next = newnode;
-    (*stack)->prev = newnode;
-    tmp = newnode;
-    i++;
-  }
+	t_node	*tmp;
+	t_node	*newnode;
+	int		i;
+
+	i = 1;
+	if (!arr[i] || !is_number(arr[i]))
+	{
+		ft_printf("Error\n");
+		free_up(*stack);
+		*stack = NULL;
+		return ;
+	}
+	*stack = malloc(sizeof(t_node));
+	if (!(*stack))
+	{
+		ft_printf("Error\n");
+		*stack = NULL;
+		return ;
+	}
+	(*stack)->data = ft_atoi(arr[1]);
+	(*stack)->distance = 0;
+	(*stack)->next = *stack;
+	(*stack)->prev = *stack;
+	tmp = *stack;
+	i = 2;
+	while (i < n)
+	{
+		if (!arr[i] || !is_number(arr[i]))
+		{
+			ft_printf("Error\n");
+			free_up(*stack);
+			*stack = NULL;
+			return ;
+		}
+		newnode = malloc(sizeof(t_node));
+		if (!newnode)
+		{
+			ft_printf("Error\n");
+			free_up(*stack);
+			*stack = NULL;
+			return ;
+		}
+		newnode->data = ft_atoi(arr[i]);
+		newnode->distance = 0;
+		newnode->next = *stack;
+		newnode->prev = tmp;
+		tmp->next = newnode;
+		(*stack)->prev = newnode;
+		tmp = newnode;
+		i++;
+	}
 }
--- sorting.c
+++ sorting.c
@@ -3,45 +3,45 @@
 /*                                                        :::      ::::::::   */
 /*   sorting.c                                          :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: ydimitra <ydimitra@student.42.f>           +#+  +:+       +#+        */
+/*   By: ydimitra <ydimitra@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/11/26 16:06:22 by ydimitra          #+#    #+#             */
-/*   Updated: 2025/12/06 17:01:57 by ydimitra         ###   ########.fr       */
+/*   Updated: 2025/12/06 17:22:38 by ydimitra         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "header.h"
 
-void sort_three(t_stack *stack_a)
+void	sort_three(t_stack *stack_a)
 {
-  t_node *max;
+	t_node	*max;
 
-  if (!*stack_a || (*stack_a)->next == *stack_a)
-      return;
-  max = max_node(*stack_a);
-  if (*stack_a == max)
-  {
-    shift_up(stack_a);
-    ft_printf("ra\n");
-  }
-  else if ((*stack_a)->next == max)
-    {
-        shift_down(stack_a);
-        ft_printf("rra\n");
-    }
-    if ((*stack_a)->data > (*stack_a)->next->data)
-    {
-        swap_first_sec(stack_a);
-        ft_printf("sa\n");
-    }
+	if (!*stack_a || (*stack_a)->next == *stack_a)
+		return ;
+	max = max_node(*stack_a);
+	if (*stack_a == max)
+	{
+		shift_up(stack_a);
+		ft_printf("ra\n");
+	}
+	else if ((*stack_a)->next == max)
+	{
+		shift_down(stack_a);
+		ft_printf("rra\n");
+	}
+	if ((*stack_a)->data > (*stack_a)->next->data)
+	{
+		swap_first_sec(stack_a);
+		ft_printf("sa\n");
+	}
 }
 
-void shift_up(t_stack *stack)
+void	shift_up(t_stack *stack)
 {
-  *stack = (*stack)->next;
+	*stack = (*stack)->next;
 }
 
-void shift_down(t_stack *stack)
+void	shift_down(t_stack *stack)
 {
-  *stack = (*stack)->prev;
+	*stack = (*stack)->prev;
 }
